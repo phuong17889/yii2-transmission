d9666923413e775cfa68ed285d3a613c7a163ca4
Lê Phương <notteen@gmail.com>
2016-04-24 01:16:41 +0700

    fix declare

--- src/components/Transmission.php
+++ src/components/Transmission.php
@@ -11,32 +11,6 @@ namespace phuong17889\transmission\components;
 class Transmission extends \Transmission\Transmission {
 
 	/**
-	 * @param int $id
-	 *
-	 * @return Torrent
-	 */
-	public function get($id) {
-		return parent::get($id);
-	}
-
-	/**
-	 * @return Torrent[]
-	 */
-	public function all() {
-		return parent::all();
-	}
-
-	/**
-	 * @param      $torrent
-	 * @param bool $metainfo
-	 *
-	 * @return \Transmission\Model\Torrent
-	 */
-	public function add($torrent, $metainfo = false) {
-		return parent::add($torrent, $metainfo);
-	}
-
-	/**
 	 * @return int
 	 */
 	public function getTotalSize() {
